5537199ae9a93d30b49178024058268b6a9aadb1
jather06 <jordhi001@gmail.com>
2024-12-10 16:06:52 +0100

    buttons en input label gemaakt en getest of werkt

--- translate_starting_code.py
+++ translate_starting_code.py
@@ -4,7 +4,24 @@ root = Tk()
 
 words_en = ["mouse", "armadillo", "caterpillar", "buffalo", "dragonfly", "eel", "platypus", "lark", "manatee", "squid"]
 words_nl = ["muis", "gordeldier", "rups", "buffel", "libelle", "paling", "vogelbekdier", "leeuwerik", "zeekoe", "inktvis"]
+def previous():
+    print("previous button clicked")
+def next():
+    print("next button clicked")
+def check():
+    print("check button clicked")
 
-# ...
+# Init buttons, labels, entries
+PrevBut = Button(root, text='Previous', command=previous)
+NextBut = Button(root, text='Next', command=next)
+CheckBut = Button(root, text='Check', command=check)
+InputField = Entry(root)
+
+
+# Arrange widgets on the screen
+PrevBut.pack()
+NextBut.pack()
+CheckBut.pack()
+InputField.pack()
 
 root.mainloop()
