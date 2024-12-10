4d02bcf98a81fea49c7ac35879a5c51f40cb2f9b
jather06 <jordhi001@gmail.com>
2024-12-10 15:57:00 +0100

    repo gemaakt

---
+++ translate_starting_code.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+root = Tk()
+
+words_en = ["mouse", "armadillo", "caterpillar", "buffalo", "dragonfly", "eel", "platypus", "lark", "manatee", "squid"]
+words_nl = ["muis", "gordeldier", "rups", "buffel", "libelle", "paling", "vogelbekdier", "leeuwerik", "zeekoe", "inktvis"]
+
+# ...
+
+root.mainloop()
